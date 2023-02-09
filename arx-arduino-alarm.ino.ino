c5c92206c6d04cdc8c94f7e83a635e59aef92351
M. Holen <53666103+holentek@users.noreply.github.com>
2023-02-09 23:13:24 +0100

    2023-02-09 23:13

--- arx-arduino-alarm.ino.ino
+++ arx-arduino-alarm.ino.ino
@@ -1,3 +1,31 @@
+const int pir1 = A0;
+const int pir2 = A1;
+const int pir3 = A2;
+int raw = 0;
+int Vin = 5;
+float Vout = 0;
+float R1 = 1000;
+float R2 = 0;
+float buffer = 0;
+
+void setup(){
+Serial.begin(9600);
+}
+
+void loop(){
+  raw = analogRead(analogPin);
+  if(raw){
+    buffer = raw * Vin;
+    Vout = (buffer)/1024.0;
+    buffer = (Vin/Vout) - 1;
+    R2= R1 * buffer;
+    Serial.print("Vout: ");
+    Serial.println(Vout);
+    Serial.print("R2: ");
+    Serial.println(R2);
+    delay(1000);
+  }
+}
 
 /*
 const int pir1 = A0;
